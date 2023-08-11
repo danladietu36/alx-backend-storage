e93396a122a45ca62eccffd5eafa8d65d248c11e
Danladi Etu <danladietugithub@example.com>
2023-08-11 22:36:33 +0000

    create table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,9 @@
-/*SQL script that creates a table users with the following 
- id, integer, never null, auto increment and primary key
- emai, string (255 characters), never null and unique
- name, string (255 characters)
- */
-
-CREATE TABLE IF NOT EXISTS users(id INT NOT NULL AUTO_INCREMENT PRIMARY kEY,
+--SQL script that creates a table users with the following 
+--id, integer, never null, auto increment and primary key
+-- emai, string (255 characters), never null and unique
+--name, string (255 characters)
+CREATE TABLE IF NOT EXISTS users(
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
  	name VARCHAR(255)
 	)
