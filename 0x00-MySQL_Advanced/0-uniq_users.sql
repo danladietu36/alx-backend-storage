7d97650f6b9bc36d6c16149ee8a6732b7c9c77aa
Danladi Etu <danladietugithub@example.com>
2023-08-11 22:38:36 +0000

    create table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -2,8 +2,8 @@
 --id, integer, never null, auto increment and primary key
 -- emai, string (255 characters), never null and unique
 --name, string (255 characters)
-CREATE TABLE IF NOT EXISTS users(
-	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	email VARCHAR(255) NOT NULL UNIQUE,
- 	name VARCHAR(255)
-	)
+CREATE TABLE IF NOT EXISTS users (
+	id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
+)
