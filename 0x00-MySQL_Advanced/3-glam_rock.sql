042186d54496640afd503cc99cf577de2e422713
Danladi Etu <danladietugithub@example.com>
2023-08-12 17:11:43 +0000

    ifnull

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -4,5 +4,5 @@
 -- You should use attributes formed and split for computing the lifespan
 SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
 	FROM metal_bands
-	WHERE style LIKE %Glam rock%
+	WHERE style LIKE '%Glam rock%'
 	ORDER BY lifespan DESC
