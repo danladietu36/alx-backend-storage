2c51d90c45f6d8b11dad659b5d95571c560de1be
Danladi Etu <danladietugithub@example.com>
2023-08-18 17:53:01 +0000

    list all documents

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" A python script that lists all documents in a collection
+"""
+
+
+import pymongo
+
+
+def list_all(mongo_collection):
+    '''function that lists all documents in a collection'''
+    if not mongo_collection:
+        return []
+    return list(mongo_collection.find())
